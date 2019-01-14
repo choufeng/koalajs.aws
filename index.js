aedafc6229b12bdd087a97ff9ce775f27cd5e929
Jon.Xia <xiajia@vip.qq.com>
2019-01-14 20:14:45 +0800

    增加数据操作

--- index.js
+++ index.js
@@ -3,13 +3,41 @@
 var fs = require('fs');
 var path = require('path');
 
-exports.get = function(event, context, callback) {
-  var contents = fs.readFileSync(`public${path.sep}index.html`);
-  var result = {
-    statusCode: 200,
-    body: contents.toString(),
-    headers: {'content-type': 'text/html'}
-  };
+const AWS = require('aws-sdk')
 
-  callback(null, 'hello,Jon');
+AWS.config.update({
+  region: "ap-southeast-1"
+})
+
+exports.get = function(event, context, callback) {
+  // var contents = fs.readFileSync(`public${path.sep}index.html`);
+
+  var docClient = new AWS.DynamoDB.DocumentClient();
+  var params = {
+    TableName : "Movies",
+  }
+  docClient.query(params, function(err, data) {
+    if (err) {
+      var result = {
+        statusCode: 500,
+        // body: contents.toString(),
+        body: 'db error',
+        headers: {'content-type': 'text/html'}
+      }
+      callback(null, 'hello,Jon');
+    } else {
+      let str = ''
+      data.Items.forEach(function(item) {
+        str += item.id
+      })
+
+      var result = {
+        statusCode: 200,
+        body: 'hello Jon: ' + str,
+        headers: {'content-type': 'text/html'}
+      };
+
+      callback(null, 'hello,Jon');
+    }
+  })
 };
